f40832ec9e072bb383b7e57f0222730778e5760f
Fabian Vogelsteller <fabian@frozeman.de>
2014-11-07 13:15:33 +0100

    released under my own name

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-    name: "mrt:reactive-timer",
+    name: "frozeman:reactive-timer",
 	summary: "A simple timer object, which can re-run reactive functions based on an interval",
-    version: "0.1.3",
+    version: "0.1.4",
     git: "https://github.com/frozeman/meteor-reactive-timer.git"
 });
 
 Package.onUse(function (api, where) {
-    api.versionsFrom('METEOR@0.9.0');
+    api.versionsFrom('METEOR@1.0');
 
     // core
-	api.use('deps', 'client');
+	api.use('tracker', 'client');
 
 
 	api.export('ReactiveTimer');
@@ -18,7 +18,7 @@ Package.onUse(function (api, where) {
 });
 
 Package.onTest(function (api) {
-	api.use('mrt:reactive-timer', 'client');
+	api.use('frozeman:reactive-timer', 'client');
 	api.use('tinytest', 'client');
 
 	api.addFiles('test/tests.js', 'client');
